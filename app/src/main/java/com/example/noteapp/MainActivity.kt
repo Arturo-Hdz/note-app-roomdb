56fe12ced1e2b155ceffbd93206254d684925b3f
Arturo-Hdz <arbaherpe07@hotmail.com>
2024-09-24 13:06:35 -0400

    commit note app room db

---
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.noteapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.noteapp.database.NoteDatabase
+import com.example.noteapp.repository.NoteRepository
+import com.example.noteapp.viewmodel.NoteViewModel
+import com.example.noteapp.viewmodel.NoteViewModelFactory
+import com.example.notesroompractice.R
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+//        setContentView(binding.root)
+
+//        enableEdgeToEdge()
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider( this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+}
